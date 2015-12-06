0c3353752d6dd989907bbebd59e93d43903ee07b
Montana Flynn <montana@montanaflynn.me>
2015-12-05 18:38:04 -0800

    Added pigLatin function

--- dist/anything.js
+++ dist/anything.js
@@ -806,6 +806,21 @@
     };
 
     anything.prototype.nothing = nothing;
+    var pigLatin = function(text) {
+        var suffix = "ay";
+        var vowels = ["a", "e", "i", "o", "u"];
+        var first = phrase.slice(0, 1);
+        var second = phrase.slice(1);
+
+        if (vowels.indexOf(first) > -1) {
+            return text + suffix;
+        } else {
+            return second + first + suffix;
+        }
+    };
+
+    anything.prototype.piglatin = pigLatin;
+
     var ping = function() {
         return "pong";
     };
